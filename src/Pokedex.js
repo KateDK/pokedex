2aea1cfd28dc5326bfd2fb96e7825915fdb07b1f
Kate Dubitski <kate.dubitski@gmail.com>
2020-05-12 17:31:38 -0400

    create and render Pokecard component

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,8 +1,13 @@
 import React from 'react';
-
+import Pokecard from './Pokecard';
 
 const Pokedex = () =>{
-  return <h1>Hello</h1>
+  return (
+    <div>
+      <h1>Pokedex</h1>
+      <Pokecard/>
+    </div>
+  )
 }
 
 export default Pokedex;
