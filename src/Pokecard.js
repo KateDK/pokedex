957747dfef1585246398754d113b7ced26dd2348
Kate Dubitski <kate.dubitski@gmail.com>
2020-05-13 15:25:39 -0400

    Add default props to Pokecard

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
-const Pokecard = () => {
+const Pokecard = (props) => {
+  console.log(">>>", props);
   return (
     <div>Pokecard</div>
   )
+}
+
+Pokecard.defaultProps = {
+  id: 25, name: 'Pikachu', type: 'electric', base_experience: 112
 }
 
 export default Pokecard;
