8989a64ed746a0291194d51ef06f7e16b9e0a6e3
Kate Dubitski <kate.dubitski@gmail.com>
2020-05-14 16:52:56 -0400

    Create dealHand function

--- src/Pokegame.js
+++ src/Pokegame.js
@@ -2,6 +2,14 @@ import React from 'react';
 import Pokedex from './Pokedex'
 import pokemons from './pokemons'
 
+const dealHand = cards => {
+  const hand = [];
+  for(let i = 0; i < 4; i++){
+    hand.push(cards[Math.floor(Math.random() * cards.length)])
+  }
+  return hand;
+}
+
 const Pokegame = props => {
 return (<h1>Pokegame</h1>)
 }
