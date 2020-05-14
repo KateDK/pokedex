edadfed969be0768962bc489cdc3bd03848a7be0
Kate Dubitski <kate.dubitski@gmail.com>
2020-05-14 16:37:15 -0400

    create and import Pokegame to App

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import Pokedex from './Pokedex'
 import pokemons from './pokemons'
+import Pokegame from './Pokegame'
 
 function App() {
   return (
     <div className="App">
       <h1>Pokedex</h1>
+      <Pokegame />
       <Pokedex pokemons={pokemons}/>
     </div>
   );
---
+++ src/Pokegame.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Pokegame = props => {
+return (<h1>Pokegame</h1>)
+}
+
+export default Pokegame;
